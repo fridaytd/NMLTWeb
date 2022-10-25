202991f13a292f6b642ac71924406c35e007ea6e
Thai Dang <81310089+fridaytd@users.noreply.github.com>
2022-10-25 10:12:02 +0700

    update

--- lab1/script.js
+++ lab1/script.js
@@ -1,12 +1,57 @@
+const search = document.forms['search']
+const txt = document.getElementById('txt_search')
+txt.addEventListener('keypress', (event) => {
+    if (event.key == 'Enter') 
+    sent_data(event)
+    
+})
 
-const search = document.getElementById('search')
-search.addEventListener('keypress', (event) => {
-    if (event.key == 'Enter') {
-        if (search.value.length == 0) {
+const sent_data = (event) => {
+     if (txt.value.length > 0) {
+            search.submit()
+        }
+        else {
             event.preventDefault()
         }
+}
+
+let emailReg = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+let passReg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#?!@$%^&*-]).{8,}/
+
+const checkMail = (mail) => {
+    if (!emailReg.test(mail.value)){
+        alert('Email khong hop le')
+        return false
+    }
+
+    return true
+}
+
+const checkPass = (pass) => {
+    if (!passReg.test(pass.value)){
+        alert('passs khong hop le')
+        return false
+    }
+
+    return true
+}
+
+const loginCheck = (login) => {
+    if (checkMail(login.email) && checkPass(login.password))
+    {
+        return true
     }
     else {
-        
+        return false
     }
-})
+}
+
+const logonCheck = (logon) => {
+    if (checkMail(logon.email) && checkPass(logon.password) && checkPass(passwordagain))
+    {
+        return true
+    }
+    else {
+        return false
+    }
+}
