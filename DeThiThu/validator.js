a518866249de644ee33abe9c67f799e48194faee
toonij <fridaytd>
2022-11-22 10:53:27 +0700

    add de thi thu

---
+++ DeThiThu/validator.js
@@ -0,0 +1,8 @@
+var mssv = document.getElementById('form');
+mssv.addEventListener('blur',(a, b)=> {
+    console.log()
+})
+
+var inputList = document.querySelectorAll('#form input')
+
+console.log(inputList);
